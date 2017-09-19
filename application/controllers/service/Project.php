7bb5dfd18587312478071469835cef88f86f933f
sara-izaap <saravanan.izaap@gmail.com>
2017-09-19 11:49:46 +0530

    Web services

--- application/controllers/service/Project.php
+++ application/controllers/service/Project.php
@@ -2,7 +2,7 @@
 
 require_once(COREPATH."libraries/REST_Controller.php");
 
-class User extends REST_Controller {
+class Project extends REST_Controller {
 	
 	
     function __construct()
@@ -12,28 +12,19 @@ class User extends REST_Controller {
         
     }
 
-    public function login_post()
+    public function list_get()
 	{
 
 		try
 		{
 			$user_data = array();
 
-			$name 		= $this->post('username');
-			$password 	= $this->post('password');
+			$contid 		= $this->get('id');
 
-			if( strcmp('', trim($name) ) === 0 || strcmp('', trim($password) ) === 0 )
+			if( strcmp('', trim($contid) ) === 0)
 				throw new Exception("Required fields are missing in your request");
 				
-
-			$user_data['data'] = $this->api_model->login( $name, $password );
-
-			if( !is_array($user_data['data']) || !count($user_data['data']) )
-			 throw new Exception("The Username or Password is invalid .");
-			
-			if( $user_data['data']['active'] == 'N' )
-			 throw new Exception("The user account has been blocked or Inactive. Please try after some time or contact administrator to resolve.");
-
+			$user_data['data'] = $this->api_model->contractor_projects($contid);
 
 			$user_data['message'] = "Login Successfully";
 			$user_data['status'] = 'SUCCESS';
--- core/models/Api_model.php
+++ core/models/Api_model.php
@@ -14,11 +14,26 @@ class Api_model extends App_Model {
 	{
 		$where=array('username'=>$name,'password'=>md5($password));
 		
-		$this->db->select("id,first_name,last_name,company_name,active,office_phone,cell_phone,email1,address1,city,state,zip",FALSE);
+		$this->db->select("id,CONCAT(first_name,' ',last_name) as name,company_name,active,office_phone,cell_phone,email1,address1,city,state,zip",FALSE);
 		$this->db->from('contractor');
 		$this->db->where($where);
 
 		$user = $this->db->get()->row_array();
+
+		return $user;
+		
+	}
+
+	function contractor_projects($id)
+	{
+		
+		$this->db->select("p.id,p.project_name,p.blueprint,p.status,CONCAT(project_address1,',',project_address2,',',project_city) as address ",FALSE);
+		$this->db->from('project p');
+		$this->db->join('project_contractors pc','p.id=pc.project_id');
+		$this->db->where("FIND_IN_SET('$id',pc.contractor_id) !=",0);
+		$this->db->group_by("p.id");
+
+		$user = $this->db->get()->result_array();
 
 		return $user;
 		
